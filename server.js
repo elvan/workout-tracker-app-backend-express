9091e70151d1cd5b4822b41f96f9156c3461bd63
Elvan Hidayat <elvanhd@gmail.com>
2023-02-27 06:39:11 +0700

    connect to the database with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
+
+mongoose.set('strictQuery', false);
 
 // express app
 const app = express();
@@ -17,7 +20,18 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(
+    process.env.MONGO_URI || 'mongodb://localhost:27017/workout-tracker-app'
+  )
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
