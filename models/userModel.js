4d33066896fc6d7eb4d0d63048468e7a4ec277dd
Elvan Hidayat <elvanhd@gmail.com>
2023-02-27 20:55:06 +0700

    email and password validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,21 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  // @ts-ignore
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid');
+  }
+
+  // @ts-ignore
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  }
+
   // @ts-ignore
   const exists = await this.findOne({ email });
 
