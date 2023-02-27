3485f42ced99ece2cc74ba50b764f291bbc65a37
Elvan Hidayat <elvanhd@gmail.com>
2023-02-28 02:21:23 +0700

    protect api routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const {
+  createWorkout,
   getWorkouts,
   getWorkout,
-  createWorkout,
   deleteWorkout,
   updateWorkout,
 } = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
 
 // GET all workouts
 router.get('/', getWorkouts);
 
-// GET a single workout
+//GET a single workout
 router.get('/:id', getWorkout);
 
 // POST a new workout
